ce307d7cb24d62bf89349465ca220f232552421c
kyle <kyledesmarais@gmail.com>
2025-07-13 17:41:13 -0400

    omnifunc map

--- nvim/lua/base/keymaps.lua
+++ nvim/lua/base/keymaps.lua
@@ -9,3 +9,6 @@ vim.keymap.set('n', '<c-k>', ':wincmd k<CR>')
 vim.keymap.set('n', '<c-j>', ':wincmd j<CR>')
 vim.keymap.set('n', '<c-h>', ':wincmd h<CR>')
 vim.keymap.set('n', '<c-l>', ':wincmd l<CR>')
+
+--omnifunc 
+vim.keymap.set('i', '<c-s>', '<c-x><c-o>')
