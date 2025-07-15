80140287835709e37caa1181f059102ad5c28048
kyle <kyledesmarais@gmail.com>
2025-07-15 16:35:14 -0400

    whitespace

--- nvim/lua/core/autocommands.lua
+++ nvim/lua/core/autocommands.lua
@@ -3,8 +3,22 @@
 --  See `:help vim.hl.on_yank()`
 vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
-  group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
+  group = vim.api.nvim_create_augroup('highlight-yank', { clear = true }),
   callback = function()
     vim.highlight.on_yank()
   end,
 })
+
+--[[ this, but lua
+highlight ExtraWhitespace ctermbg=red guibg=red
+match ExtraWhitespace /\s\+$/
+au BufWinEnter * match ExtraWhitespace /\s\+$/
+au InsertEnter * match ExtraWhitespace /\s\+\%#\@<!$/
+au InsertLeave * match ExtraWhitespace /\s\+$/
+au BufWinLeave * call clearmatches()
+" Remove all trailing whitespaces
+nnoremap <silent> <leader>rs :let _s=@/ <Bar> :%s/\s\+$//e <Bar> :let @/=_s <Bar> :nohl <Bar> :unlet _s <CR>
+]]
+
+local ws_hl_group = vim.api.nvim_set_hl(0, "ExtraWhitespace", { fg="red", bg="red"})
+local ws_group = vim.api.nvim_create_augroup('highlight-ws', { clear = true })
