fce878e0e1d94daf94a8f15b9240ce4fe1e2f6cc
kyle <kyledesmarais@gmail.com>
2025-07-15 16:36:36 -0400

    bleh

--- nvim/lua/core/load_plugins.lua
+++ nvim/lua/core/load_plugins.lua
@@ -1,8 +1,10 @@
 require("lazy").setup({
   "ellisonleao/gruvbox.nvim",
+  "tpope/vim-commentary",
   "lewis6991/gitsigns.nvim",
   "nvim-lualine/lualine.nvim",
   "mason-org/mason.nvim",
+  "ThePrimeagen/vim-be-good",
   { "nvim-treesitter/nvim-treesitter",
     build = ":TSUpdate",
     main = "nvim-treesitter.configs",
