430fd6c43b9c1332447bda22491cdf0143322327
kyle <kyledesmarais@gmail.com>
2025-08-15 11:49:04 -0400

    bleh

--- install_base.sh
+++ install_base.sh
@@ -28,7 +28,7 @@ function installNeovim() {
     cd ~/nvim
     wget ${neo_url}
     tar xvzf nvim-linux-x86_64.tar.gz -C ~/.local/share/
-    # pv -s $(stat -c %s nvim-linux-x86_64.tar.gz) ./nvim-linux-x86_64.tar.gz | tar xz
+    # pv -s $(stat -c %s archive.tar.gz) archive.tar.gz | tar -xzv -f - | awk '{printf "\rExtracting: %s", $0; fflush();} END{print ""}'
     mkdir -p ~/.local/bin
     cd ~/.local/bin/
     ln -sf ~/.local/share/nvim-linux-x86_64/bin/nvim nvim
